37e7dbe0ba57b92a787e1c4c43f7b97de8991ef6
Takashi Miwa <sanrinsha.tm@gmail.com>
2011-10-25 20:06:39 +0000

    完成

--- replaceWikiHash2h6/replaceWikiHash2h6.php
+++ replaceWikiHash2h6/replaceWikiHash2h6.php
@@ -1,8 +1,12 @@
 <?php
-$handle = @fopen("/tmp/inputfile.txt", "r");
+//Wikiで#を使ってリスト表示していたものをh6. <番号>.に変えるプログラム
+$handle = @fopen("./inputfile.txt", "r");
 if ($handle) {
+    $i = 1;
     while (($buffer = fgets($handle, 4096)) !== false) {
-        echo $buffer;
+        echo preg_replace('/^#[^!]/', "h6. $i.", $buffer, 1, $count);
+        //echo "$buffer";
+        $i += $count;
     }
     if (!feof($handle)) {
         echo "Error: unexpected fgets() fail\n";
